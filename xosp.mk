17895000b7a883d4f7588db52f1c235ff5ba30ed
nilac8991 <nilac8991@gmail.com>
2016-02-14 12:12:58 +0100

    Refactor to XOSP
    Signed-off-by: nilac8991 <nilac8991@gmail.com>

--- xosp.mk
+++ xosp.mk
@@ -2,12 +2,12 @@
 TARGET_SCREEN_HEIGHT := 720
 TARGET_SCREEN_WIDTH := 1280
 
-# Inherit some common Reborn stuff.
+# Inherit some common XOSP stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/reborn/config/common_full_phone.mk)
+$(call inherit-product, vendor/xosp/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/reborn/config/reborn.mk)
+$(call inherit-product, vendor/xosp/config/xosp.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/Z008/device.mk)
@@ -17,12 +17,12 @@ DEVICE_PACKAGE_OVERLAYS += device/asus/Z008/overlay
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := reborn_Z008
+PRODUCT_NAME := xosp_Z008
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_Z008
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z008
-RELEASE_TYPE := REBORN_OFFICIAL
+RELEASE_TYPE := XOSP_OFFICIAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=WW_Z008 \
